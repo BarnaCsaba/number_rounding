b9848e338699f9d831cd0a801abb833048472732
Barna Csaba <barna.csaba@diak.szbi-pg.hu>
2026-02-13 09:21:23 +0100

    5

--- src/number_rounding.py
+++ src/number_rounding.py
@@ -1,5 +1,5 @@
 def round_number(num, decimals):
-    if not isinstance(num, (int, float)) or not isinstance(decimals, int):
+    if not isinstance(num, (float, int)) or not isinstance(decimals, int):
         raise TypeError("Invalid input")
     factor = 10 ** decimals
     return round(num * factor) / factor
